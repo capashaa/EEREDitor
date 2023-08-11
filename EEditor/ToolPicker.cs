c047082c5af0bab008477f7ee16a43785c288ba7
capashaa <130944764+capashaa@users.noreply.github.com>
2023-08-12 01:48:26 +0200

    Exact color

--- EEditor/ToolPicker.cs
+++ EEditor/ToolPicker.cs
@@ -14,15 +14,23 @@ namespace EEditor
             ToolpickerSettings settings = new ToolpickerSettings();
             if (settings.ShowDialog() == DialogResult.OK)
             {
-                ColorDialog cd = new ColorDialog();
-                if (cd.ShowDialog() == DialogResult.OK)
+                if (!string.IsNullOrEmpty(settings.hex))
                 {
-                    Color2ID(cd.Color);
+                    Color color = ColorTranslator.FromHtml(settings.hex);
+                    Color2ID(color,settings.colorExact);
+                }
+                else
+                {
+                    ColorDialog cd = new ColorDialog();
+                    if (cd.ShowDialog() == DialogResult.OK)
+                    {
+                        Color2ID(cd.Color, settings.colorExact);
+                    }
                 }
             }
         }
 
-        private void Color2ID(Color color)
+        private void Color2ID(Color color,bool exactly)
         {
             if (MainForm.userdata.ColorFG)
             {
@@ -44,48 +52,41 @@ namespace EEditor
                     }
                 }
             }
-            var bid = closestColor(color);
+            var bid = closestColor(color,exactly);
             editArea.MainForm.setBrick(bid,true);
         }
 
-        private int closestColor(Color col)
+        private int closestColor(Color col,bool exactly)
         {
             int black = 0;
             double num = 999.0;
             double d = 0.0;
             foreach (KeyValuePair<Color,int> id in colblocks)
             {
-                Color color2 = id.Key;
-                d = 0.0;
-                if (color2.R >= col.R)
+                if (exactly)
                 {
-                    d += Math.Pow((double)(color2.R - col.R), 2.0);
+                    if (col == id.Key)
+                    {
+                        black = id.Value;
+                        break;
+                    }
                 }
                 else
                 {
-                    d += Math.Pow((double)(col.R - color2.R), 2.0);
-                }
-                if (color2.G >= col.G)
-                {
-                    d += Math.Pow((double)(color2.G - col.G), 2.0);
-                }
-                else
-                {
-                    d += Math.Pow((double)(col.G - color2.G), 2.0);
-                }
-                if (color2.B >= col.B)
-                {
-                    d += Math.Pow((double)(color2.B - col.B), 2.0);
-                }
-                else
-                {
-                    d += Math.Pow((double)(col.B - color2.B), 2.0);
-                }
-                d = Math.Sqrt(d);
-                if (d < num)
-                {
-                    num = d;
-                    black = id.Value;
+                    Color color2 = id.Key;
+                    d = 0.0;
+                    if (color2.R >= col.R) d += Math.Pow((double)(color2.R - col.R), 2.0);
+                    else d += Math.Pow((double)(col.R - color2.R), 2.0);
+                    if (color2.G >= col.G) d += Math.Pow((double)(color2.G - col.G), 2.0);
+                    else d += Math.Pow((double)(col.G - color2.G), 2.0);
+                    if (color2.B >= col.B) d += Math.Pow((double)(color2.B - col.B), 2.0);
+                    else d += Math.Pow((double)(col.B - color2.B), 2.0);
+                    d = Math.Sqrt(d);
+                    if (d < num)
+                    {
+                        num = d;
+                        black = id.Value;
+                    }
                 }
             }
             return black;
